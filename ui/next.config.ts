fd7c9c5f59cd8da799af102c24fcb8ec0ba6e55c
Keyhole-Koro <korokororinthird@gmail.com>
2026-05-05 15:34:46 +0900

    feat: add turbopack configuration to next.js setup since failed to build

--- ui/next.config.ts
+++ ui/next.config.ts
@@ -1,9 +1,13 @@
 import type { NextConfig } from 'next';
+import path from 'node:path';
 
 const nextConfig: NextConfig = {
   transpilePackages: ['@synthify/proto-ts'],
   experimental: {
     externalDir: true,
+  },
+  turbopack: {
+    root: path.join(__dirname, '..', '..', '..'),
   },
   output: 'standalone',
   images: { unoptimized: true },
